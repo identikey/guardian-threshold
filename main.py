470b969c3f705bea5374b045318b219a0a9c86ac
Duke Jones <104690+dukejones@users.noreply.github.com>
2024-10-06 08:20:42 -0700

    fix api running

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Any, Generator
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
@@ -5,13 +6,27 @@ import models
 
 models.Base.metadata.create_all(bind=engine)
 
-def get_db():
+app = FastAPI()  # Add this line to create the FastAPI app instance
+
+
+def get_db() -> Generator[Session, Any, None]:
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
 
+
 @app.get("/status")
-def read_status():
+def read_status() -> dict[str, str]:
     return {"status": "OK"}
+
+
+def start() -> None:
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    start()
